3811f6f8012379ca2f1b5e699979d587ff6115a8
Jon Rogers <jon@carbonfive.com>
2020-06-30 07:42:52 -0700

    Be a little smarter about the slack bot.
    This PR now only starts the slackbot if SLACK_API_TOKEN has been set.
    
    This allows us to run the CLI by it self if we want from the escript
    generated executable.

--- lib/slack_color_theme_generator/app.ex
+++ lib/slack_color_theme_generator/app.ex
@@ -5,21 +5,26 @@ defmodule SlackColorThemeGenerator.App do
   use Supervisor
 
   def start(_type, _args) do
-    children = [
-      %{
-        id: Slack.Bot,
-        start: {
-          Slack.Bot, :start_link, [
-            SlackColorThemeGenerator.SlackBot, [], System.get_env("SLACK_API_TOKEN")
-          ]
+    token = System.get_env("SLACK_API_TOKEN")
+    if (token) do
+      children = [
+        %{
+          id: Slack.Bot,
+          start: {
+            Slack.Bot, :start_link, [
+              SlackColorThemeGenerator.SlackBot, [], System.get_env("SLACK_API_TOKEN")
+            ]
+          }
         }
-      }
-    ]
+      ]
 
-    opts = [strategy: :one_for_one, name: Supervisor]
+      opts = [strategy: :one_for_one, name: Supervisor]
 
-    Logger.info( fn -> "Starting SlackBot..." end )
-    Supervisor.start_link(children, opts)
+      Logger.info( fn -> "Starting SlackBot..." end )
+      Supervisor.start_link(children, opts)
+    else
+      {:error, :ignore}
+    end
   end
 
 end
