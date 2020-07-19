54f1f9e27b81ae4e36a79a355621307b5bcf1439
Jon Rogers <jon@carbonfive.com>
2020-07-19 11:40:49 -0700

    Split supervisor and application into separate files
    Previously the `App` was acting as both Application and Supervisor.
    
    Though that works, feels better to keep them separated.

---
+++ lib/slack_color_theme_generator/supervisor.ex
@@ -0,0 +1,16 @@
+defmodule SlackColorThemeGenerator.Supervisor do
+  use Supervisor
+
+  def init(:ok) do
+    token = System.get_env("SLACK_API_TOKEN")
+    children = [
+      ImageProcessor,
+      token && {SlackBotWrapper, [token]}
+    ] |> Enum.filter(&(&1))
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+end
